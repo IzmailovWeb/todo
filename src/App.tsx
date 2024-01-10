291ba9ab85c696d55ec5d780134dcf57c0d8e173
IzmailovWeb <izmailovweb@gmail.com>
2024-01-10 23:11:20 +0300

    add todo components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {TodoList} from "./components/todoList/TodoList";
+import styled from "styled-components";
+import {v1} from "uuid";
+
+
+export type filterValuesType = "all"| "completed"| "active";
 
 function App() {
+
+  let [task, setTask] = useState([
+    {id: v1(), title: "CSS", isDone: true},
+    {id: v1(), title: "JS", isDone: true},
+    {id: v1(), title: "REACT", isDone: false},
+    {id: v1(), title: "Redax", isDone: false},
+  ]);
+
+  function changeFilter(value:filterValuesType){
+    setFilter(value)
+
+  }
+  function addTask( title:string) {
+    let newTask = { id:v1() , title:title, isDone:false }
+    let newTasks = [newTask, ...task]
+    setTask(newTasks)
+  }
+
+
+
+  let [filter,setFilter] = useState<filterValuesType>("all")
+
+  let filtredTasksForTodoList = task
+  if (filter === "completed") {
+    filtredTasksForTodoList = task.filter( t => (t.isDone === true))
+  }
+  if (filter === "active") {
+    filtredTasksForTodoList = task.filter( t => (t.isDone === false))
+  }
+
+
+  function removeTask(id: string) {
+    let filteredTasks = task.filter(t => t.id !== id)
+    setTask(filteredTasks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Wrapper>
+        <TodoList title="What to learn"
+                  tasks={filtredTasksForTodoList}
+                  removeTask={removeTask}
+                  changeFilter = {changeFilter}
+                  addTask = {addTask}
+        />
+        {/*<TodoList title="Movies" tasks = {tasks2}/>*/}
+      </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex`
---
+++ src/components/todoList/TodoList.tsx
@@ -0,0 +1,80 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import styled from "styled-components";
+import {filterValuesType} from "../../App";
+
+
+type TasksPropsType = {
+    id: string,
+    title: string,
+    isDone: boolean
+}
+
+
+type TodoListPropsType = {
+    title: string
+    tasks: TasksPropsType[]
+    removeTask: (taskId: string) => void
+    changeFilter: (value: filterValuesType) => void
+    addTask: (title: string) => void
+};
+export const TodoList = (props: TodoListPropsType) => {
+    const [newTaskTite, setNewTaskTitle] = useState("");
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>)=>{
+        setNewTaskTitle(e.currentTarget.value)
+    }
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>)=>{
+        if (e.charCode === 13) {
+            props.addTask(newTaskTite)
+            setNewTaskTitle("")
+        }
+    }
+    const addTask = ()=>{
+        props.addTask(newTaskTite)
+        setNewTaskTitle("")
+    }
+
+    const onAllClickHandler = ()=> props.changeFilter("all")
+    const onActiveClickHandler = ()=> props.changeFilter("active")
+    const onCompletedClickHandler = ()=> props.changeFilter("completed")
+    return (
+        <Card>
+            <h3>{props.title}</h3>
+            <div>
+                <input value={newTaskTite} onChange={onChangeHandler} onKeyPress={onKeyPressHandler}/>
+                <button onClick={addTask}>+</button>
+            </div>
+            <List>
+                {
+                    props.tasks.map((t,) => {
+                        const onRemoveHandler = ()=>{props.removeTask(t.id)}
+                        return (
+                            <Item key={t.id}>
+                                <input type={"checkbox"} checked={t.isDone}/>
+                                <span>{t.title} </span>
+                                <button onClick={onRemoveHandler}>x</button>
+                            </Item>
+                        )
+                    })
+                }
+            </List>
+            <div>
+                <button onClick={onAllClickHandler}>All</button>
+                <button onClick={onActiveClickHandler}>Active</button>
+                <button onClick={onCompletedClickHandler}>Completed</button>
+            </div>
+        </Card>
+    );
+};
+
+const Card = styled.div`
+    width: 200px;
+`
+
+const List = styled.ul`
+    padding: unset;
+`
+
+const Item = styled.li`
+    list-style: none;
+`
